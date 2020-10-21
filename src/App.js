bd2603a741ecb6e6565d0bdb2b2af6b58a72a313
ifs-gisilk <gihan.siriwardhana@ifsworld.com>
2020-10-21 19:37:42 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import './App.css';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 
 class App extends Component {
@@ -16,6 +17,8 @@ class App extends Component {
             <Redirect to="/404" />
           </Switch>
         </BrowserRouter>
+
+        <Footer/>
       </div>
     );
   }
